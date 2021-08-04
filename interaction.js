39d29e8b35e8abf3051059c10332a78625a6abcb
Hector L. Santiago <hlsp@me.com>
2021-08-04 11:09:16 -0500

    javascript1

--- interaction.js
+++ interaction.js
@@ -1,8 +1,9 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-
+modal-link
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
+var img2 = document.getElementById("myImg2");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
