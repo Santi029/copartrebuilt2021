54bdac6244c2fd1f6e1920b56f3ff22644735365
Hector L. Santiago <hlsp@me.com>
2021-08-10 16:55:57 -0500

    Creating the second page gallery participants.

--- interaction.js
+++ interaction.js
@@ -105,3 +105,54 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal6.style.display = "none";
 }
+
+// Get the modal 7
+var modal7 = document.getElementById("myModal7");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img7 = document.getElementById("myImg7");
+
+img7.onclick = function(){
+  modal7.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal7.style.display = "none";
+}
+
+// Get the modal 8
+var modal8 = document.getElementById("myModal8");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img8 = document.getElementById("myImg8");
+
+img6.onclick = function(){
+  modal8.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal8.style.display = "none";
+}
+
+// Get the modal 9
+var modal9 = document.getElementById("myModal9");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img9 = document.getElementById("myImg9");
+
+img6.onclick = function(){
+  modal9.style.display = "block";
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal9.style.display = "none";
+}
